ef64aebb91ed1a5dc802901e2747703f4e0ea6d7
RHermanet14 <rhermanet14@gmail.com>
2025-08-20 18:21:51 -0400

    started detecting keyboard input

--- KvmSwitch.Client/Services/MouseService.cs
+++ KvmSwitch.Client/Services/MouseService.cs
@@ -12,6 +12,7 @@ namespace services
         #region Private Variables
         private const uint RID_INPUT = 0x10000003;
         private const uint RIM_TYPEMOUSE = 0;
+        private const uint RIM_TYPEKEYBOARD = 1;
         private const uint RIDEV_REMOVE = 0x00000001;
         private const uint RIDEV_INPUTSINK = 0x00000100;
         private const int WM_INPUT = 0x00FF;
@@ -57,6 +58,18 @@ namespace services
         {
             public RAWINPUTHEADER header;
             public RAWMOUSE mouse; // union { RAWMOUSE mouse; RAWKEYBOARD keyboard; RAWHID hid; }
+            public RAWKEYBOARD keyboard;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWKEYBOARD
+        {
+            public ushort MakeCode;
+            public ushort Flags;
+            public ushort Reserved;
+            public ushort VKey;
+            public uint Message;
+            public ulong ExtraInformation;
         }
         #endregion
 
@@ -91,12 +104,17 @@ namespace services
         {
             if (_registered)
                 return true;
-            RAWINPUTDEVICE[] devices = new RAWINPUTDEVICE[1];
+            RAWINPUTDEVICE[] devices = new RAWINPUTDEVICE[2];
             devices[0].usUsagePage = 0x01;
             devices[0].usUsage = 0x02;
             devices[0].dwFlags = RIDEV_INPUTSINK;
             devices[0].hwndTarget = Handle;
-            if (!RegisterRawInputDevices(devices, 1, (uint)Marshal.SizeOf(typeof(RAWINPUTDEVICE))))
+
+            devices[1].usUsagePage = 0x01;
+            devices[1].usUsage = 0x06;
+            devices[1].dwFlags = RIDEV_INPUTSINK;
+            devices[1].hwndTarget = Handle;
+            if (!RegisterRawInputDevices(devices, 2, (uint)Marshal.SizeOf(typeof(RAWINPUTDEVICE))))
             {
                 Console.WriteLine("Failed to register device");
                 return false;
@@ -130,17 +148,24 @@ namespace services
                     {
                         // Read the header first
                         RAWINPUTHEADER header = Marshal.PtrToStructure<RAWINPUTHEADER>(buffer);
-                        
+                        int headerSize = Marshal.SizeOf<RAWINPUTHEADER>();
+                        IntPtr DataPtr = IntPtr.Add(buffer, headerSize);
+
                         if (header.dwType == RIM_TYPEMOUSE)
                         {
                             // Calculate offset to mouse data (after header)
-                            int headerSize = Marshal.SizeOf<RAWINPUTHEADER>();
-                            IntPtr mouseDataPtr = IntPtr.Add(buffer, headerSize);
+                            //int headerSize = Marshal.SizeOf<RAWINPUTHEADER>();
+                            //IntPtr mouseDataPtr = IntPtr.Add(buffer, headerSize);
 
                             // Read mouse data directly
-                            RAWMOUSE mouseData = Marshal.PtrToStructure<RAWMOUSE>(mouseDataPtr);
+                            RAWMOUSE mouseData = Marshal.PtrToStructure<RAWMOUSE>(DataPtr);
 
                             UpdateMovementData(mouseData.usButtonFlags, mouseData.usButtonData, mouseData.lLastX, mouseData.lLastY);
+                        }
+                        else if (header.dwType == RIM_TYPEKEYBOARD)
+                        {
+                            RAWKEYBOARD keyboardData = Marshal.PtrToStructure<RAWKEYBOARD>(DataPtr);
+                            Console.WriteLine($"{keyboardData.MakeCode}, {keyboardData.Flags}");
                         }
                     }
                 }
@@ -175,13 +200,17 @@ namespace services
         {
             if (!_registered) return true;
 
-            RAWINPUTDEVICE[] devices = new RAWINPUTDEVICE[1];
+            RAWINPUTDEVICE[] devices = new RAWINPUTDEVICE[2];
             devices[0].usUsagePage = 0x01;
             devices[0].usUsage = 0x02;
             devices[0].dwFlags = RIDEV_REMOVE;
             devices[0].hwndTarget = IntPtr.Zero;
 
-            bool success = RegisterRawInputDevices(devices, 1, (uint)Marshal.SizeOf(typeof(RAWINPUTDEVICE)));
+            devices[1].usUsagePage = 0x01;
+            devices[1].usUsage = 0x06;
+            devices[1].dwFlags = RIDEV_REMOVE;
+            devices[1].hwndTarget = IntPtr.Zero;
+            bool success = RegisterRawInputDevices(devices, 2, (uint)Marshal.SizeOf(typeof(RAWINPUTDEVICE)));
             
             if (success)
             {
