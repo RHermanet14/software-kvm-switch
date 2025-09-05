8b8cad86dc6ede83523a1769eda7594551854123
RHermanet14 <rhermanet14@gmail.com>
2025-09-05 17:59:38 -0400

    Added more controls to server interface.

--- KvmSwitch.ServerUI/ServerUI.Designer.cs
+++ KvmSwitch.ServerUI/ServerUI.Designer.cs
@@ -28,18 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            PortLabel = new Label();
+            PortTextBox = new TextBox();
+            StartButton = new Button();
+            StopButton = new Button();
+            IPLabel = new Label();
+            ToggleButton = new Button();
+            SaveButton = new Button();
+            toolTip1 = new ToolTip(components);
             SuspendLayout();
+            // 
+            // PortLabel
+            // 
+            PortLabel.AutoSize = true;
+            PortLabel.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PortLabel.Location = new Point(252, 14);
+            PortLabel.Name = "PortLabel";
+            PortLabel.Size = new Size(73, 20);
+            PortLabel.TabIndex = 0;
+            PortLabel.Text = "Enter Port";
+            // 
+            // PortTextBox
+            // 
+            PortTextBox.Location = new Point(331, 11);
+            PortTextBox.Name = "PortTextBox";
+            PortTextBox.Size = new Size(84, 23);
+            PortTextBox.TabIndex = 1;
+            PortTextBox.KeyPress += PortTextBox_KeyPress;
+            // 
+            // StartButton
+            // 
+            StartButton.Location = new Point(259, 103);
+            StartButton.Name = "StartButton";
+            StartButton.Size = new Size(75, 23);
+            StartButton.TabIndex = 2;
+            StartButton.Text = "Start Server";
+            StartButton.UseVisualStyleBackColor = true;
+            StartButton.Click += StartButton_Click;
+            // 
+            // StopButton
+            // 
+            StopButton.Location = new Point(340, 103);
+            StopButton.Name = "StopButton";
+            StopButton.Size = new Size(75, 23);
+            StopButton.TabIndex = 3;
+            StopButton.Text = "Stop Server";
+            StopButton.UseVisualStyleBackColor = true;
+            StopButton.Click += StopButton_Click;
+            // 
+            // IPLabel
+            // 
+            IPLabel.AutoSize = true;
+            IPLabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            IPLabel.Location = new Point(39, 38);
+            IPLabel.Name = "IPLabel";
+            IPLabel.Size = new Size(155, 37);
+            IPLabel.TabIndex = 4;
+            IPLabel.Text = "192.168.1.1";
+            IPLabel.Click += IPLabel_Click;
+            IPLabel.MouseMove += IPLabel_MouseMove;
+            // 
+            // ToggleButton
+            // 
+            ToggleButton.Location = new Point(12, 11);
+            ToggleButton.Name = "ToggleButton";
+            ToggleButton.Size = new Size(218, 24);
+            ToggleButton.TabIndex = 5;
+            ToggleButton.Text = "Toggle Display IP";
+            ToggleButton.UseVisualStyleBackColor = true;
+            ToggleButton.Click += ToggleButton_Click;
+            // 
+            // SaveButton
+            // 
+            SaveButton.Location = new Point(331, 40);
+            SaveButton.Name = "SaveButton";
+            SaveButton.Size = new Size(84, 23);
+            SaveButton.TabIndex = 6;
+            SaveButton.Text = "Save Port";
+            SaveButton.UseVisualStyleBackColor = true;
+            SaveButton.Click += SaveButton_Click;
             // 
             // ServerUI
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(427, 138);
+            Controls.Add(SaveButton);
+            Controls.Add(ToggleButton);
+            Controls.Add(IPLabel);
+            Controls.Add(StopButton);
+            Controls.Add(StartButton);
+            Controls.Add(PortTextBox);
+            Controls.Add(PortLabel);
             Name = "ServerUI";
             Text = "Server Interface";
+            Load += ServerUI_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label PortLabel;
+        private TextBox PortTextBox;
+        private Button StartButton;
+        private Button StopButton;
+        private Label IPLabel;
+        private Button ToggleButton;
+        private Button SaveButton;
+        private ToolTip toolTip1;
     }
 }
--- KvmSwitch.ServerUI/ServerUI.cs
+++ KvmSwitch.ServerUI/ServerUI.cs
@@ -6,5 +6,55 @@ namespace ServerUI
         {
             InitializeComponent();
         }
+
+        private void ToggleButton_Click(object sender, EventArgs e)
+        {
+            IPLabel.Visible = !IPLabel.Visible;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            StartButton.Enabled = false;
+            StopButton.Enabled = true;
+        }
+
+        private void ServerUI_Load(object sender, EventArgs e)
+        {
+            StopButton.Enabled = false;
+            IPLabel.Visible = false;
+            PortTextBox.Text = KvmSwitch.ServerUI.Properties.Settings.Default.Port;
+
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            StartButton.Enabled = true;
+            StopButton.Enabled = false;
+        }
+
+        private void IPLabel_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(IPLabel.Text);
+            MessageBox.Show("IP Address Was Copied!");
+        }
+
+        private void PortTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8) // char 8 = Backspace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            KvmSwitch.ServerUI.Properties.Settings.Default.Port = PortTextBox.Text;
+            MessageBox.Show("Preference Was Saved.");
+        }
+
+        private void IPLabel_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip1.SetToolTip(IPLabel, "Click To Copy IP To Clipboard.");
+        }
     }
 }
